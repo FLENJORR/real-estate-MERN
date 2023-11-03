007d8d3240c8254cd7bf1e1388ac43c953331501
flenjorr <flenjorr@gmail.com>
2023-11-03 16:27:25 +0100

    sign in functionality

--- API/controllers/auth.controller.js
+++ API/controllers/auth.controller.js
@@ -16,12 +16,20 @@ try {
     res.status(201).json('user creeated successfully !');
     
 } catch (error) {
-    next(error);
-    
+    next(error);   
 }
-    
-
-
-
-
 };
+
+
+// signIn functionality
+export const signin =async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+    } catch (error) {
+        next(error);
+    }
+}
